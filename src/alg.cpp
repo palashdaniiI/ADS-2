be98d89c8ac0f246383e270dac67daeee82d09df
palashdaniiI <99319050+palashdaniiI@users.noreply.github.com>
2022-03-06 17:38:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,46 @@
 
 
 double pown(double value, uint16_t n) {
-
+  double a = 1;
+  for (int i = 0; i < n; i++) {
+    a *= value;
+  }
+  return a;
 }
 
 uint64_t fact(uint16_t n) {
-
+  if ((n == 1) || (n == 0)) {
+    return 1;
+  }
+  else {
+    return n * fact(n - 1);
+  }
 }
 
 double calcItem(double x, uint16_t n) {
-
+  return pown(x, n)/fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+  double sum = 0;
+  for (int i = 0; i <= count; i++) {
+    sum += calcItem(x, i);
+  }
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-
+  double a = 0;
+  for (int i = count; i > 0; i--) {
+    a += pown(-1, u-1) * calcItem(x, w)) + sinn(x, q);
+  }
+  return a;
 }
 
 double cosn(double x, uint16_t count) {
-
+  double a = 0;
+  for (int i = count; i > 0; i--) {
+    a += pown(-1, j-1) * calcItem(x, 2 * q) + cosn(x, q);
+  }
+  return a;
 }
